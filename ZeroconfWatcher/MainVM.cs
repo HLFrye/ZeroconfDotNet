668bb55edc8005361f37b87cd18a46dcd084ec57
Harvey Frye <hlfrye@gmail.com>
2015-06-02 22:59:55 -0500

    Continuing work on service viewer client

---
+++ ZeroconfWatcher/MainVM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Net.NetworkInformation;
+
+namespace ZeroconfWatcher
+{
+    class MainVM
+    {
+        public ObservableCollection<NetworkTab> Networks { get; set; }
+
+        public MainVM()
+        {
+            Networks = new ObservableCollection<NetworkTab>(EnumerateNetworks());
+        }
+
+        IEnumerable<NetworkTab> EnumerateNetworks()
+        {
+            //Create all-network first
+            //yield return new NetworkTab("All");
+
+            var ip4networks = NetworkInterface.GetAllNetworkInterfaces()
+                .Where(x => x.SupportsMulticast && !x.IsReceiveOnly && x.OperationalStatus == OperationalStatus.Up)
+                .Where(x => (x.GetIPProperties().UnicastAddresses.Count > 0))
+                .Where(x => x.GetIPProperties().UnicastAddresses
+                                               .Select(y => y.Address)
+                                               .Where(y => (y.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) || (y.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6))
+                                               .Count() > 0)
+                .ToList();
+
+            for (var i = 0; i < ip4networks.Count; ++i)
+            {
+                var name = string.Format("Network {0}", i+1);
+                yield return new NetworkTab(ip4networks[i], name);
+            }
+
+            //var ip6networks = NetworkInterface.GetAllNetworkInterfaces()
+            //    .Where(x => x.SupportsMulticast && !x.IsReceiveOnly && x.OperationalStatus == OperationalStatus.Up)
+            //    .SelectMany(x => x.GetIPProperties().UnicastAddresses)
+            //    .Where(x => x.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+            //    .Select(x =>
+            //        new NetworkTab(x.Address.ToString())).ToList();
+
+            //foreach (var net in ip6networks)
+            //{
+            //    yield return net;
+            //}
+
+        }
+    }
+}
---
+++ ZeroconfWatcher/NetworkTab.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Net;
+using System.Net.NetworkInformation;
+
+using ZeroconfDotNet;
+
+namespace ZeroconfWatcher
+{
+    class NetworkTab
+    {
+        public NetworkTab(NetworkInterface iface, string name)
+        {
+            TabCaption = name;
+            Description = iface.Name;
+            IP4Address = iface.GetIPProperties().UnicastAddresses.Where(x => x.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).Select(x => x.Address.ToString()).First();
+            IP6Address = iface.GetIPProperties().UnicastAddresses.Where(x => x.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6).Select(x => x.Address.ToString()).First();
+
+            TabContent = new FrameworkElement();
+        }
+
+        public string TabCaption { get; set; }
+        public string Description { get; set; }
+        public string IP4Address { get; set; }
+        public string IP6Address { get; set; }
+        public FrameworkElement TabContent { get; set; }
+        public ObservableCollection<SearchInfo> Searches { get; set; }
+
+        private object _service;
+
+        public void AddSearch(string name)
+        {
+
+        }
+
+
+        public class SearchInfo
+        {
+            public string Protocol { get; set; }
+            public ObservableCollection<ServiceInfo> Services { get; set; }
+        }
+    }
+}
